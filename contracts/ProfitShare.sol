d28f74b12504f1e75b1b5c88c961a33cd8356384
Adam Flesher <adam.flesher@gmail.com>
2018-05-04 15:38:22 -0700

    More tests

--- contracts/ProfitShare.sol
+++ contracts/ProfitShare.sol
@@ -15,11 +15,23 @@ contract ProfitShare {
 
     uint public outstandingVotes;
     uint public outstandingShares;
+    uint public lastPayout;
+    uint public minPayoutBalance = 1 ether;
+    uint public payoutCooldown = 23 hours + 30 minutes;
 
     mapping (address => Shareholder) public shareholders;
+    address[] public owners;
     
-    constructor() public {
+    constructor() public payable {
         
+    }  
+
+    function percent(uint numerator, uint denominator, uint precision) public pure returns(uint quotient) {
+            // caution, check safe-to-multiply here
+        uint _numerator = numerator * 10 ** (precision+1);
+        // with rounding of last digit
+        uint _quotient = ((_numerator / denominator) + 5) / 10;
+        return ( _quotient);
     }
 
     function _removeShareholder(address _shareholder) internal {
@@ -27,16 +39,41 @@ contract ProfitShare {
         outstandingVotes = outstandingVotes.sub(shareholder.votes);
         outstandingShares = outstandingShares.sub(shareholder.shares);
         delete shareholders[_shareholder];
+        // remove shareholder from array!!
     }
 
     function _addShareholder(address _shareholder, uint32 _votes, uint32 _shares) internal {
         shareholders[_shareholder] = Shareholder(_votes, _shares);
         outstandingVotes = outstandingVotes.add(_votes);
         outstandingShares = outstandingShares.add(_shares);
+        owners.push(_shareholder);
     }
 
     function addShareholder(address _shareholder, uint32 _votes, uint32 _shares) external {
         _removeShareholder(_shareholder);
         _addShareholder(_shareholder, _votes, _shares);
+    }
+
+    function removeShareholder(address _shareholder) external {
+        _removeShareholder(_shareholder);
+    }
+
+    // `fallback` function called when eth is sent to Payable contract
+    function () public payable {
+    }
+
+    function disburse() external {
+        // check payout cooldown
+        // itterate shareholders
+        // get shareholder pct / total shares
+        // transfer balance based on pct
+        require((now > lastPayout + payoutCooldown) && (address(this).balance > minPayoutBalance));
+        for (uint i = 0; i < owners.length; i++) {
+            address owner = owners[i];
+            Shareholder storage shareholder = shareholders[owner];
+            uint share = (percent(shareholder.shares, outstandingShares, 4) * address(this).balance) / 10000;
+            owner.transfer(share * address(this).balance);
+        }
+
     }
 }
--- test/TestProfitShare.sol
+++ test/TestProfitShare.sol
@@ -5,9 +5,12 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/ProfitShare.sol";
 
 contract TestProfitShare {
-    function testAddShareholder () public {
-        ProfitShare profitShare = ProfitShare(DeployedAddresses.ProfitShare());
 
+    ProfitShare profitShare = ProfitShare(DeployedAddresses.ProfitShare());
+
+    address address1 = 0xf4455A889fa6A6B4dd588045437c4f16cAA5445a;
+
+    function testAddShareholder () public {
         uint expectedVotes = 10000;
         uint expectedShares = 5;
         profitShare.addShareholder(msg.sender, uint32(expectedVotes), uint32(expectedShares));
@@ -19,5 +22,32 @@ contract TestProfitShare {
         Assert.equal(profitShare.outstandingShares(), expectedShares, "Adding shareholder should update outstanding shares");
         Assert.equal(uint(votes), expectedVotes, "Adding shareholder should update shareholder votes");
         Assert.equal(uint(shares), expectedShares, "Adding shareholder should update shareholder shares");
+    }
+
+    function testRemoveShareholder() public {
+        uint startVotes = profitShare.outstandingVotes();
+        uint startShares = profitShare.outstandingShares();
+
+        profitShare.addShareholder(address1, uint32(5000), uint32(4000));
+        profitShare.removeShareholder(address1);
+
+        Assert.equal(profitShare.outstandingVotes(), startVotes, "Removing shareholder should update outstanding votes");
+        Assert.equal(profitShare.outstandingShares(), startShares, "Removing shareholder should update outstanding shares");
+    }
+
+    function testUtilities() public {
+        uint numerator = 11;
+        uint denominator = 5;
+        uint precision = 1;
+
+        Assert.equal(profitShare.percent(numerator, denominator, precision), 22, "Division");
+        Assert.equal(profitShare.percent(5, 11, 2), 45, "Division 2");
+        Assert.equal((profitShare.percent(5, 11, 2) * 5000) / uint(100), 2250, "Division 3");
+    }
+
+    function testDisburse() public {
+        address(profitShare).transfer(5 wei);
+
+        Assert.balanceEqual(address(profitShare), 5 wei, "Balance Set");
     }
 }
